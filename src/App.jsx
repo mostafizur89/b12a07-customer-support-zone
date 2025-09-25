cac3f27beb94a9f69be25307e14bccc5e124dfd2
mostafizur89 <mr4020089@gmail.com>
2025-09-25 12:54:35 +0600

    added navber section

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    {/* header section ----> */}
+    <div className="border-2 flex justify-between max-w-[1200px] mx-auto">
+
+     <div>
+       <a className="btn btn-ghost text-xl font-bold ">CS — Ticket System</a>
+     </div>
+      
+      <div className='flex justify-around items-center'>
+         <div className=''>
+           <ul className="menu menu-horizontal px-1">
+             <li><a>Home</a></li>
+             <li><a>FAQ</a></li>
+             <li><a>Changelog</a></li>
+              <li><a>Blog</a></li>
+              <li><a>Download</a></li>
+              <li><a>Contact</a></li>
+           </ul>
+         </div>
+         <div className="">
+            <a className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white">+ New Ticket</a>
+         </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+    </div>
+
+    {/* banner section------> */}
+
+    <div className='max-w-[1200px] mx-auto flex justify-between items-center gap-3 my-[40px]'>
+
+      <div className='border-2 w-1/2 h-[250px] flex justify-center items-center rounded-lg '>
+        <div>
+          <h5 className='font-semibold text-2xl'>In-Progress</h5>
+          <h2 className='flex justify-center items-center text-5xl font-bold'>0</h2>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='border-2 w-1/2 h-[250px] flex justify-center items-center rounded-lg'>
+        <div className=''>
+          <h5 className='font-semibold text-2xl'>Resolve</h5>
+          <h2 className='flex justify-center items-center text-5xl font-bold'>0</h2>
+        </div>
+      </div>
+
+    </div>
+   
+  
+
     </>
   )
 }
